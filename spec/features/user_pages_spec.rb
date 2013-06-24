714f133c1cbca82567d322f1ae7f6084e1755f5b
Dave Ferris <dave@entrision.com>
2013-06-24 13:08:27 -0500

    Add user following

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -57,21 +57,21 @@ describe "User pages" do
   end
 
   describe "profile page" do
-	let(:user) { FactoryGirl.create(:user) }
-  let!(:m1) { FactoryGirl.create(:micropost, user: user, content: 'Post number one') }
-  let!(:m2) { FactoryGirl.create(:micropost, user: user, content: 'El numero dos') }
+  	let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: 'Post number one') }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: 'El numero dos') }
 
-	before { visit user_path(user) }
+  	before { visit user_path(user) }
 
-	it { should have_selector('h1', text: user.name) }
-	it { should have_title(user.name) }
+  	it { should have_selector('h1', text: user.name) }
+  	it { should have_title(user.name) }
 
-  describe "microposts" do
-    it { should have_content(m1.content) }
-    it { should have_content(m2.content) }
-    it { should have_content(user.microposts.count) }
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
-end
 
   describe "signup" do
 
@@ -156,6 +156,34 @@ end
       it { should have_link('Sign out', href: signout_path) }
       specify { user.reload.name.should  == new_name }
       specify { user.reload.email.should == new_email }
+    end
+  end
+
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      it { should have_title(full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
+
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      it { should have_title(full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
     end
   end
 end
