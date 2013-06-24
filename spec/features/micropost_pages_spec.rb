5f37665a5637164224d6eeb2510b94a6a644d410
Dave Ferris <dave@entrision.com>
2013-06-24 10:04:02 -0500

    Complete Ch 10 Exercises

--- spec/features/micropost_pages_spec.rb
+++ spec/features/micropost_pages_spec.rb
@@ -41,5 +41,45 @@ describe "Micropost pages" do
         expect { click_link "delete" }.to change(Micropost, :count).by(-1)
       end
     end
+
+    describe 'as incorrect user' do
+      let(:user2) { FactoryGirl.create(:user) }
+      let!(:m1) { FactoryGirl.create(:micropost, user: user2) }
+
+      before do
+        sign_in user
+        visit root_path
+      end
+
+      describe 'delete link should not be present' do
+        it { should_not have_content('Delete') }
+      end
+    end
+  end
+
+  describe "for user with one micropost" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) {FactoryGirl.create(:micropost, user: user, content: 'Post number one') }
+
+    before do
+      sign_in user
+      visit root_path
+    end
+
+    it { should have_content('micropost') }
+    it { should_not have_content('microposts') }
+  end
+
+  describe "for user with multiple microposts" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) {FactoryGirl.create(:micropost, user: user, content: 'Post number one') }
+    let!(:m2) {FactoryGirl.create(:micropost, user: user, content: 'Post number two') }
+
+    before do
+      sign_in user
+      visit root_path
+    end
+
+    it { should have_content('microposts') }
   end
 end
